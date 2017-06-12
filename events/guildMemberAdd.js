1006948e4156fdd7e8b4431bbc4c8776f1adfeeb
The Real Isvan <andrewmmrnc@gmail.com>
2017-06-12 18:34:45 +0300

    Create guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = member => {
   let guild = member.guild;
-  guild.defaultChannel.send(`Va rog sa il primiti pe ${member.user.username} in acest server !`);
+  guild.defaultChannel.send(`Va rog sa il/o primiti pe ${member.user.username} in acest server !`);
 };
