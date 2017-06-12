3af6a78e3e7357b0a99e7cbc355e7c26f7b1e81e
The Real Isvan <andrewmmrnc@gmail.com>
2017-06-12 23:43:17 +0300

    Create eu.js

--- comenzi/eu.js
+++ comenzi/eu.js
@@ -3,6 +3,7 @@ exports.run = (client, msg) => {
   • Bot realizat de therealisvan#6449 .
  • Versiune bot : 1.3.8
  • Construit pe baza Discord.js
+ • Repo GitHub : https://github.com/andrewmmrnc/Shiromino /// fork me on GitHub .
  • Discord therealisvan#6449 : https://discordapp.com/invite/JTFTvF6
  • Pentru intrebari nu ezitati sa intrati pe discordul meu .
 `;
