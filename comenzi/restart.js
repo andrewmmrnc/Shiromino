63055f8faca21a5a73681bfa4c7329036fac9dd9
The Real Isvan <andrewmmrnc@gmail.com>
2017-06-17 02:36:01 +0300

    Create restart.js

---
+++ comenzi/restart.js
@@ -0,0 +1,22 @@
+exports.run = (client, msg) => {
+  msg.channel.sendMessage("Adorm ...")
+    .then(() => {
+      process.exit();
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'restart',
+  description: 'Restarteaza bot-ul .',
+  usage: 'restart'
+};
